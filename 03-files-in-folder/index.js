299bd1911fb26cabb8e8ee771da75a75953caa39
Alexander Martinovich <alexander_martinovich@msn.com>
2022-11-04 02:08:55 +0200

    feat: add task 3 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+let directoryPath = path.join(__dirname, 'secret-folder');
+
+fs.readdir(directoryPath, {withFileTypes: true}, (err, files) => {
+  if (err) {console.log(err.message)}
+  files.forEach(file => {
+    if (file.isFile()) {
+      fs.stat(path.join(__dirname, 'secret-folder', file.name), (err, stat) => {
+        if (err) {console.log(err.message)}
+        console.log(`${file.name.split('.')[0]} - ${file.name.split('.')[1]} - ${stat.size/1000}kb`)
+      })
+    }
+  })
+});
+
