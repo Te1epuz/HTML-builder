b1736f52f0aa7f36d1b5dfb2579b5344469123eb
Alexander Martinovich <alexander_martinovich@msn.com>
2022-11-05 01:10:43 +0200

    refactor: total refactor of task 4 solution

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,45 +1,33 @@
 const fs = require('fs');
 const path = require('path');
 
-let directoryPath = path.join(__dirname, 'files');
-let newDirectoryPath = path.join(__dirname, 'files-copy');
+let folderPath = path.join(__dirname, 'files');
+let newFolderPath = path.join(__dirname, 'files-copy');
 
-function makeFolder() {
-  fs.mkdir(newDirectoryPath, { recursive: true }, (err) => {
-    if (err) {console.log(err.message)}
-  });
-};
-
-function deleteFiles() {
-  fs.promises.readdir(newDirectoryPath, {withFileTypes: true})
-    .then ((files) => {
-      files.forEach(file => {
-        if (file.isDirectory()) {
-          console.log('Recursive deleting of folder was NOT in the task!')
-        }
-        if (file.isFile()) {
-          fs.unlink(path.join(newDirectoryPath, file.name), (err) => {
-            if (err) {console.log(err.message)}
-          });
-        }
-      })
-    })
+async function makeFolder(newFolderPath){
+  await fs.promises.rm(newFolderPath, { recursive: true, force: true });
+  await fs.promises.mkdir(newFolderPath, { recursive: true });
 }
 
-function copyFiles() {
-  fs.promises.readdir(directoryPath, {withFileTypes: true})
-    .then((files) => {
-      files.forEach(file => {
-        let sourceFile = path.join(directoryPath, file.name);
-        let destFile = path.join(newDirectoryPath, file.name);
-        fs.copyFile(sourceFile, destFile, (err) => {
-          if (err) {console.log(err.message)}
-        });
-      })
-    })
-};
+async function copyFiles(folderPath, newFolderPath){
+  let files = await fs.promises.readdir(folderPath, {withFileTypes: true})
+  for (let file of files) {
+    if (file.isDirectory()) {
+      let OldFolder = path.join(folderPath, file.name);
+      let newFolder = path.join(newFolderPath, file.name);
+      await fs.promises.mkdir(newFolder, { recursive: true });
+      await copyAssets(OldFolder, newFolder);
+    }
+    else {
+      let sourceFile = path.join(folderPath, file.name);
+      let destFile = path.join(newFolderPath, file.name);
+      await fs.promises.copyFile(sourceFile, destFile);
+    }
+  }
+}
 
-
-makeFolder();
-deleteFiles();
-copyFiles();
+async function copyFolder() {
+  await makeFolder(newFolderPath);
+  await copyFiles(folderPath, newFolderPath);
+}
+copyFolder()
